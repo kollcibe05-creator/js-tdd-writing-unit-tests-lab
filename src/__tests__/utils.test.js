044664d433b2e9fbfbe85db321e391cfb868bb4f
Collins Kibet <kollcibe05@gmail.com>
2025-10-29 13:56:35 +0300

    general test for palindrome

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,16 @@
 // Your tests here
+import {isPalindrome} from "../utils";
+
+test("return true if a word is a palindrome and false if not", ()=> {
+    
+   const palindrome = "racecar" 
+   const notPalindrome = "word"
+
+   const palindromeAnswer = isPalindrome("racecar")
+   const palindromeNotAnswer = isPalindrome("word")
+
+   expect(palindromeAnswer).toBeTruthy();
+   expect(palindromeNotAnswer).toBeFalsy();
+})
+
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
 // Your code here
+
+export function isPalindrome (word) {
+    for(let i = 0; i<word.length/2; i++) {
+        let j = word.length-1-i
+
+        return word[i] === word[j];
+    }
+
+}
